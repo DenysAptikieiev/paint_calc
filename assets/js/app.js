69852f114f19215580934e03661acf73325b869e
Denys <rtan333@gmail.com>
2021-02-13 18:07:02 +0200

    price commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,35 +3,44 @@
 const kilogram = document.querySelector('#quantity'),
     btnCalculateFasad = document.querySelector('.btnF'),
     btnCalculateInter = document.querySelector('.btnInter'),
+    elemTotalPrice = document.querySelector('.total-price'),
     valueText = document.querySelector('.value-text');
 
 const rawMaterials = {
-    dispersion: { fasad: 0.25, interior: 0.085 },              //  Дисперсия
-    pigment: { fasad: 0.17857143, interior: 0.1 },             //  Диоксид Титана
-    filler: { fasad: 0.3, interior: 0.425 },                   //  Кальцит
-    preservative: { fasad: 0.00014286, interior: 0.00014286 }, //  Консервант
-    thickener: { fasad: 0.00285714, interior: 0.00285714 },    //  Загуститель
-    defoamer: { fasad: 0.00014286, interior: 0.00014286 },     //  Пеногаситель
-    coaliscent: { fasad: 0.00085714, interior: 0.00085714 }    //  Коалисцент
+    dispersion: { fasad: 0.25, interior: 0.085, price: 48 },                //  Дисперсия
+    pigment: { fasad: 0.17857143, interior: 0.1, price: 80 },               //  Диоксид Титана
+    filler: { fasad: 0.3, interior: 0.425, price: 5.6 },                    //  Кальцит
+    preservative: { fasad: 0.00014286, interior: 0.00014286, price: 25.35 },//  Консервант
+    thickener: { fasad: 0.00285714, interior: 0.00285714, price: 48 },      //  Загуститель
+    defoamer: { fasad: 0.00014286, interior: 0.00014286, price: 25 },       //  Пеногаситель
+    coaliscent: { fasad: 0.00085714, interior: 0.00085714, price: 90 }      //  Коалисцент
 }
 
-const calcFunction = (type) => {
+const calcFunction = (type, price) => {
     const value = +kilogram.value;
     kilogram.value = '';
+    let totalPrice = 0;
 
     for (let key in rawMaterials) {
-        let elem = document.querySelector(`.${key}`);
-        let quantity = rawMaterials[key][type]
+
+        let elem = document.querySelector(`.${key} td:nth-child(3)`);
+        let elemOfPrice = document.querySelector(`.${key} td:nth-child(4)`);
+        let quantity = rawMaterials[key][type];
+        let priceOfElem = rawMaterials[key][price];
 
         if (!isNaN(value) && value !== ' ' && value <= 140 && value !== 0) {
             valueText.textContent = `Для ${value}kg (${type}) краски подребуется:`;
 
             let resultG = value * quantity * 1000;
             let resultK = value * quantity;
+            let resultEqualsPrice = resultK * priceOfElem;
+            totalPrice = totalPrice + resultEqualsPrice;
+            resultEqualsPrice = resultEqualsPrice.toFixed(2);
             resultK = resultK.toFixed(2);
             resultG = resultG.toFixed(2);
 
             resultK >= 1 ? elem.textContent = `${resultG}g / ${resultK}kg` : elem.textContent = `${resultG}g`;
+            elemOfPrice.textContent = `${resultEqualsPrice}UAH`
 
         } else if (value > 140) {
             kilogram.value = '';
@@ -41,7 +50,11 @@ const calcFunction = (type) => {
             kilogram.placeholder = 'введите число'
         }
     }
+    totalPrice = totalPrice.toFixed(2);
+    elemTotalPrice.textContent = `${totalPrice}UAH`;
+    // console.log('totalPrice: ', totalPrice);
+
 }
 
-btnCalculateFasad.addEventListener('click', () => calcFunction('fasad'));
-btnCalculateInter.addEventListener('click', () => calcFunction('interior'));
+btnCalculateFasad.addEventListener('click', () => calcFunction('fasad', 'price'));
+btnCalculateInter.addEventListener('click', () => calcFunction('interior', 'price'));
